cdc5232a476e77e0b6b020563e7c4ddc6b4e4fa3
Pedro Maciel <Pedro.Maciel@ecmwf.int>
2022-10-14 14:43:34 +0100

    Work

--- gb-sort.cc
+++ gb-sort.cc
@@ -44,13 +44,12 @@ void print(const T& v) {
 
 
 struct midpoint_t {
-    midpoint_t(double _point, int _meta = 0) : point(_point), meta(_meta) {}
-    double point;
-    int meta;
+    midpoint_t(double _x = 0., int _i = 0) : x(_x), i(_i) {}
+    double x;
+    int i;
 
-    bool operator<(const midpoint_t& m) const { return point < m.point || (point == m.point && meta < m.meta); }
     friend std::ostream& operator<<(std::ostream& out, const midpoint_t& m) {
-        out << m.point << '/' << m.meta;
+        out << m.x << '/' << m.i;
         return out;
     }
 };
@@ -70,6 +69,23 @@ void linear_spacing_n(ForwardIt first, size_t count, double _a, double _b, bool 
     for (size_t i = 0; i < count; ++i, ++first) {
         *first = _a + i * dx;
     }
+};
+
+
+void fill_midpoints(iterator_t<std::vector<midpoint_t>>& first, const std::vector<double>& points, double a, double b,
+                    int label) {
+    // Note: advances first by points.size() + 1
+    // Note: assumes constant increment between point coordinates
+    assert(1 < points.size());
+
+    const auto it = first;
+    const auto dx = (points[1] - points.front()) / 2.;
+    for (const auto& x : points) {
+        *first++ = {x - dx, label};
+    }
+
+    *it      = {a, label};
+    *first++ = {b, label};
 };
 
 
@@ -101,6 +117,11 @@ struct Area : protected std::array<double, 4> {
         return N() == other.N() && W() == other.W() && S() == other.S() && E() == other.E();
     }
 
+    bool includesNorthPole() const { return N() == 90.; }
+    bool includesSouthPole() const { return S() == -90.; }
+    bool isPeriodicWestEast() const { return E() == W() + 360.; }
+    bool isGlobal() const { return includesNorthPole() && includesSouthPole() && isPeriodicWestEast(); }
+
     double N() const { return operator[](0); }
     double W() const { return operator[](1); }
     double S() const { return operator[](2); }
@@ -113,57 +134,73 @@ static const Area GLOBE(GLOBE_STR);
 
 
 struct Grid {
+    virtual ~Grid() = default;
+
     static Grid* build(const std::string& name, const Area&);
 
     size_t Nj() const { return N_.size(); }
     size_t Ni(size_t j) const { return N_[j]; }
     const Area& area() const { return area_; }
 
+    virtual std::vector<double> Xj() const = 0;
+
+    std::vector<double> Xi(size_t j) const {
+        auto N = Ni(j);
+        std::vector<double> x(N);
+        linear_spacing_n(x.begin(), N, area_.W(), area_.E(), !area_.isPeriodicWestEast());
+        return x;
+    }
+
 protected:
     Grid(const Area& area) : area_(area) {}
 
     const Area area_;
     std::vector<size_t> N_;
-    std::vector<double> Xj_;
 };
 
 
-struct OGrid : Grid {
-    OGrid(size_t N, const Area& area) : Grid(area) {
+struct GaussianGrid : Grid {
+protected:
+    using Grid::Grid;
+
+    std::vector<double> Xj() const override {
+        auto N = Nj() / 2;
+        assert(0 < N && N * 2 == Nj());
+
+        std::vector<double> x(2 * N);
+        auto a  = x.begin();
+        auto b  = x.rbegin();
+        auto dx = 90. / static_cast<double>(N);
+        for (size_t i = 0; i < N; ++i, ++a, ++b) {
+            *a = 90. - dx * (i + 0.5);  // just an approximation
+            *b = -*a;
+        }
+
+        return x;
+    }
+};
+
+
+struct OGrid : GaussianGrid {
+    OGrid(size_t N, const Area& area) : GaussianGrid(area) {
+        assert(area == GLOBE);  // Note: for simplicity
         assert(N > 0);
 
         N_.resize(2 * N);
-        auto Na = N_.begin();
-        auto Nb = N_.rbegin();
-        for (size_t i = 0; i < N; ++i, ++Na, ++Nb) {
-            *Na = *Nb = 20 + i * 4;
-        }
-
-        Xj_.resize(2 * N);
-        auto Xa = Xj_.begin();
-        auto Xb = Xj_.rbegin();
-        auto dx = 90. / static_cast<double>(N);
-        for (size_t i = 0; i < N; ++i, ++Xa, ++Xb) {
-            *Xa = 90. - dx * (i + 0.5);  // just an approximation
-            *Xb = -*Xa;
+        auto a = N_.begin();
+        auto b = N_.rbegin();
+        for (size_t i = 0; i < N; ++i, ++a, ++b) {
+            *a = *b = 20 + i * 4;
         }
     }
 };
 
 
-struct FGrid : Grid {
-    FGrid(size_t N, const Area& area) : Grid(area) {
+struct FGrid : GaussianGrid {
+    FGrid(size_t N, const Area& area) : GaussianGrid(area) {
+        assert(area == GLOBE);  // Note: for simplicity
         assert(N > 0);
         N_.assign(2 * N, 4 * N);
-
-        Xj_.resize(2 * N);
-        auto Xa = Xj_.begin();
-        auto Xb = Xj_.rbegin();
-        auto dx = 90. / static_cast<double>(N);
-        for (size_t i = 0; i < N; ++i, ++Xa, ++Xb) {
-            *Xa = 90. - dx * (i + 0.5);  // just an approximation
-            *Xb = -*Xa;
-        }
     }
 };
 
@@ -172,9 +209,12 @@ struct LLGrid : Grid {
     LLGrid(size_t Ni, size_t Nj, const Area& area) : Grid(area) {
         assert(Ni > 0 && Nj > 0);
         N_.assign(Ni, Nj);
+    }
 
-        Xj_.resize(Nj);
-        linear_spacing_n(Xj_.begin(), Nj, area.N(), area.S(), true);
+    std::vector<double> Xj() const override {
+        std::vector<double> x(Nj());
+        linear_spacing_n(x.begin(), Nj(), area_.N(), area_.S(), true);
+        return x;
     }
 };
 
@@ -187,13 +227,11 @@ Grid* Grid::build(const std::string& name, const Area& area) {
     std::smatch match;  // Note: first sub_match is the whole string
     if (std::regex_match(name, match, octahedral)) {
         assert(match.size() == 2);
-        assert(area == GLOBE);  // Note: for simplicity
         return new OGrid(static_cast<size_t>(std::stol(match[1].str())), area);
     }
 
     if (std::regex_match(name, match, regular_gg)) {
         assert(match.size() == 2);
-        assert(area == GLOBE);  // Note: for simplicity
         return new FGrid(static_cast<size_t>(std::stol(match[1].str())), area);
     }
 
@@ -216,9 +254,9 @@ int main(int argc, const char* argv[]) {
 
 
         parser->add_options()("h,help", "Print help");
-        parser->add_options()("input-grid", "Input grid", cxxopts::value<std::string>()->default_value("O12"));
+        parser->add_options()("input-grid", "Input grid", cxxopts::value<std::string>()->default_value("O9"));
         parser->add_options()("input-area", "Input area", cxxopts::value<std::string>()->default_value(GLOBE_STR));
-        parser->add_options()("output-grid", "Output grid", cxxopts::value<std::string>()->default_value("O6"));
+        parser->add_options()("output-grid", "Output grid", cxxopts::value<std::string>()->default_value("O45"));
         parser->add_options()("output-area", "Output area", cxxopts::value<std::string>()->default_value(GLOBE_STR));
 
         parser->parse_positional({"input-grid", "output-grid"});
@@ -230,32 +268,33 @@ int main(int argc, const char* argv[]) {
             return 0;
         }
 
-        // build input and output grids
 
-        auto Ai = options["input-area"].as<std::string>();
-        auto Ao = options["output-area"].as<std::string>();
+        // input and output grids
 
-        std::unique_ptr<Grid> Gi(Grid::build(options["input-grid"].as<std::string>(), Area(Ai)));
-        std::unique_ptr<Grid> Go(Grid::build(options["output-grid"].as<std::string>(), Area(Ao)));
+        auto Ain  = options["input-area"].as<std::string>();
+        auto Aout = options["output-area"].as<std::string>();
 
-        // build test data to sort
+        std::unique_ptr<Grid> Gin(Grid::build(options["input-grid"].as<std::string>(), Area(Ain)));
+        std::unique_ptr<Grid> Gout(Grid::build(options["output-grid"].as<std::string>(), Area(Aout)));
 
-        std::vector<midpoint_t> m;
-        for (double i : {1.1, 1.2}) {
-            for (int j : {3, 1}) {
-                m.emplace_back(midpoint_t{i, j});
-            }
-        }
 
-        print(m);
-        std::sort(m.begin(), m.end());
-        print(m);
+        // Grid-box latitude edges (j-direction midpoints)
+        std::vector<midpoint_t> Mj(Gin->Nj() + 1 + Gout->Nj() + 1);
 
-        std::vector<double> v = {1, 3, 5, 2, 4, 6};
-        print(v);
+        auto it = Mj.begin();
+        fill_midpoints(it, Gin->Xj(), Gin->area().N(), Gin->area().S(), 0);
+        fill_midpoints(it, Gout->Xj(), Gout->area().N(), Gout->area().S(), 1);
+        assert(it == Mj.end());
 
-        std::inplace_merge(v.begin(), v.begin() + 3, v.end());
-        print(v);
+        print(Mj);
+        std::cout << "---" << std::endl;
+
+        std::inplace_merge(
+            Mj.begin(), Mj.begin() + Gin->Nj() + 1, Mj.end(),
+            [](const midpoint_t& a, const midpoint_t& b) { return a.x > b.x || (a.x == b.x && a.i < b.i); });
+
+        print(Mj);
+        std::cout << "---" << std::endl;
     }
     catch (const cxxopts::exceptions::exception& e) {
         std::cout << "error parsing options: " << e.what() << std::endl;
